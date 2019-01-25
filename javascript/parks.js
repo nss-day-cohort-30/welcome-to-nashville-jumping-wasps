baabba18b4f73222c4e9e325d2429df741e93a58
Sam Cronin <croninsw@gmail.com>
2019-01-25 14:13:40 -0600

    quick save before Dominick breaks my code

--- javascript/parks.js
+++ javascript/parks.js
@@ -1,19 +1,31 @@
-// SAM'S FILE STAY OUT
-
 let parkInputEl = document.querySelector("#parkLookup").value
 const parkButton = document.querySelector("#parkLookupButton")
+let filters = []
+let parks = []
 
 parkButton.addEventListener("click", function(){
-    const parkInputEl = document.querySelector("#parkLookup").target.id
+    const parkInputEl = document.querySelector("#parkLookup")
+    console.log(parkInputEl)
     getParksStuff(parkInputEl)
 })
+// const parkInputEl = document.querySelector("#parkLookup")
 
 const getParksStuff = (id) => {
 fetch(`https://data.nashville.gov/resource/xbru-cfzi.json?${id}=Yes&$$app_token=jnv0TSHBEnWnvTmofx6UjW0U5`)
     .then(parkData => parkData.json())
     .then(parsedParkData => {
+        let counter = 0
         parsedParkData.forEach(park => {
-                document.querySelector("#displayResultsSection").innerHTML += createParkDom(park)
+            let parkObject = {
+                name: park.park_name, 
+                address: park.mapped_location_address
+            }
+            // push returned park into array & save for later
+            parks.push(parkObject);
+            if (counter < 4) {
+            document.querySelector("#displayResultsSection").innerHTML += createParkDom(park)
+            counter++
+            }
         });
     })
 }
