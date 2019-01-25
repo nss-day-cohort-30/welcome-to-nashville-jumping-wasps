838385e3510e4d419c264e26181b7a92aca266be
Sam Cronin <croninsw@gmail.com>
2019-01-25 15:28:53 -0600

    commented code and began work on superObject

--- javascript/parks.js
+++ javascript/parks.js
@@ -1,17 +1,19 @@
+// variables and arrays
 let parkInputEl = document.querySelector("#parkLookup").value
 const parkButton = document.querySelector("#parkLookupButton")
 let filters = []
 let parks = []
 
+// dropdown menu with event listener
 parkButton.addEventListener("click", function () {
     const parkInputEl = document.getElementById("parkLookup").selectedIndex
     const key = document.getElementById("parkLookup")[parkInputEl].id
     console.log(key)
-    getParksStuff(key)
+    parkInfo(key)
 })
-// const parkInputEl = document.querySelector("#parkLookup")
 
-const getParksStuff = (id) => {
+// fetch request and dom builder
+const parkInfo = (id) => {
     fetch(`https://data.nashville.gov/resource/xbru-cfzi.json?${id}=Yes&$$app_token=jnv0TSHBEnWnvTmofx6UjW0U5`)
         .then(parkData => parkData.json())
         .then(parsedParkData => {
@@ -21,22 +23,33 @@ const getParksStuff = (id) => {
                     name: park.park_name,
                     address: park.mapped_location_address
                 }
-                // push returned park into array & save for later
-                parks.push(parkObject);
+                parks.push(parkObject)
+                
                 if (counter < 4) {
                     document.querySelector("#displayResultsSection").innerHTML += createParkDom(park)
                     counter++
                 }
             });
         })
+    // const parentCont = document.querySelector("#displayResultsSection")
+    // parentCont.addEventListener("click", function() {
+    //     itineraryObject.park = park
+    // })
 }
 
+// dom creater
+// TODO would love to make this prettier, my god
 const createParkDom = (park) => {
     return `
     <div class="domParkDiv">
         <p class="domParkEl">${park.park_name} at ${park.mapped_location_address}</p>
-        
+        <button id="${park.park_name}--id" class="saveButton">Save</button>
     </div>
     `
 }
 
+// TO DO MONDAY
+// ------------
+// 1. Breathe
+// 2. Make "parks" array work correctly. Right now it is not storing variables.
+// 3. Push parks array object to super object
