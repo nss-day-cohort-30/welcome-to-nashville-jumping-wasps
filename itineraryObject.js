83fb0a740fd0f2a07ab158eda8a18b055f7b3ab7
Sam Cronin <croninsw@gmail.com>
2019-01-28 16:47:56 -0600

    hardcoded css, fixed superObject

--- itineraryObject.js
+++ itineraryObject.js
@@ -15,15 +15,15 @@ let itineraryObject = {
         
         let html = "";
         if (this.park.name != null) {
-            html += `<p>${this.park.name}</p>`;
+            html += `<p>Park: ${this.park.name}</p>`;
         }
         if (this.restaurant.name != null) {
-            html += ` <p>${this.restaurant.name}</p>`;
+            html += ` <p>Restaurant: ${this.restaurant.name}</p>`;
         } if (this.meetup.name != null) {
-            html +=  `<p>${this.meetup.name}</p>`;
+            html +=  `<p>Meetup: ${this.meetup.name}</p>`;
         }
         if (this.concert.name != null) {
-            html +=  `<p>${this.concert.name}</p>`;
+            html +=  `<p>Concert: ${this.concert.name}</p>`;
         }
         document.querySelector("#itinerary").innerHTML = html;    
 
